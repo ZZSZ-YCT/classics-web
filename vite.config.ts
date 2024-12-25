85ad8d980cb40d0d5b65db2e3660abf916f5e799
Black <black-hig@outlook.com>
2024-12-25 17:51:14 +0800

    :sparkles: Introduce obfuscator

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vitePluginBundleObfuscator from 'vite-plugin-bundle-obfuscator';
+
+const obfuscatorConfig = {
+  autoExcludeNodeModules: true,
+  threadPool: true,
+};
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      vitePluginBundleObfuscator(obfuscatorConfig)
+  ],
   build: {
     rollupOptions:{
       input: {
