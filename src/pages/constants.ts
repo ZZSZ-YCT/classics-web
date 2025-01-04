b86aa23571c8b8aa678395244a01a299ffa79d8d
Black <black-hig@outlook.com>
2025-01-04 15:02:53 +0800

    :wrench: API URL environment var

--- src/pages/constants.ts
+++ src/pages/constants.ts
@@ -1,3 +1,3 @@
-export const base_api_url = "https://classics.shittim.art/api"
+export const base_api_url = import.meta.env.VITE_API_URL
 
 export const hitokoto = "https://international.v1.hitokoto.cn"
