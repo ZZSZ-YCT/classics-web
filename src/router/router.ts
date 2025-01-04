782e1d3e3ef6dd2c18e274a7d885fbfcb227c76b
Black <black-hig@outlook.com>
2025-01-04 14:16:51 +0800

    :beers: Revert some changes

--- src/router/router.ts
+++ src/router/router.ts
@@ -4,7 +4,7 @@ import {createRouter, createWebHistory} from "vue-router";
 
 const routes = [
     {
-        path: '/',
+        path: '/:catchAll(.*)',
         name: 'Home',
         component: Article,
         meta: {
@@ -20,10 +20,6 @@ const routes = [
                 }
             }
         ]
-    },
-    {
-        path: '/:catchAll(.*)',
-        redirect: '/'
     },
 ]
 
