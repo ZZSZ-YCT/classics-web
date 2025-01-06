3ab325a7b9ebb887a701bcdb9098f8ad3fcb7395
Black <black-hig@outlook.com>
2025-01-06 21:47:26 +0800

    :construction: Articles Fetching

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module']
+  modules: ['vuetify-nuxt-module', '@pinia/nuxt'],
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1.0',
+    }
+  },
+  runtimeConfig: {
+    public: {
+      apiUrl: 'https://classics-api.shittim.art/'
+    },
+  },
 })
